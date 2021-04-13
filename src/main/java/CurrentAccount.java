0902b520c2dade01491f0947a1f72ad4fe204e82
Maciek Kacz <maciek.kacz@wp.pl>
2021-04-13 18:45:56 +0200

    add new class CurrentAccount

---
+++ src/main/java/CurrentAccount.java
@@ -0,0 +1,9 @@
+import java.math.BigDecimal;
+
+public class CurrentAccount extends AbstractAccount {
+
+
+    public CurrentAccount(String name, BigDecimal balance, AccountType accountType, BigDecimal bigDecimal) {
+        super(name, balance, accountType);
+    }
+}
