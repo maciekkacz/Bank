b9c4f0877d634c7324f85899d7e0da3f928a292c
Maciek Kacz <maciek.kacz@wp.pl>
2021-04-13 18:56:49 +0200

    add constructor in class

--- src/main/java/CurrentAccount.java
+++ src/main/java/CurrentAccount.java
@@ -2,8 +2,21 @@ import java.math.BigDecimal;
 
 public class CurrentAccount extends AbstractAccount {
 
+    public CurrentAccount() {
+        super();
+        this.accountType = AccountType.CURRENT;
+    }
 
-    public CurrentAccount(String name, BigDecimal balance, AccountType accountType, BigDecimal bigDecimal) {
-        super(name, balance, accountType);
+    public CurrentAccount(String number, BigDecimal balance, AccountType accountType) {
+        super(number, balance, accountType);
+    }
+
+    @Override
+    public String toString() {
+        return "CurrentAccount{" +
+                "number='" + number + '\'' +
+                ", balance=" + balance +
+                ", accountType=" + accountType +
+                '}';
     }
 }
